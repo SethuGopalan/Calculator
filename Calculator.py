1a4613063d4365b9014fc0d12c10515e5fb9ef1d
Sethu Gopalan <sethugopalan@outlook.com>
2024-10-17 17:42:19 -0400

    layout update

--- Calculator.py
+++ Calculator.py
@@ -117,4 +117,4 @@ def update_display(num_clicks, operator_clicks, stored_value, stored_operator, c
 
 # Start the Dash app server
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,host="0.0.0.0",port=8050)
